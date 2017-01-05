7243ab950dd11ee99a760b96ef01dc1da3e0c66a
Camilo Rivera <Camilo.AR.Damelines@nab.com.au>
2017-01-05 10:33:35 -0500

    Initial commit for user-service

---
+++ user-service/src/main/groovy/UserService.groovy
@@ -0,0 +1,12 @@
+package user
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UserService {
+
+  static void main(String... args) {
+    SpringApplication.run(UserService, args)
+  }
+}
---
+++ user-service/src/main/groovy/user/controller/UserController.groovy
@@ -0,0 +1,24 @@
+package user.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+
+@RestController
+@RequestMapping('/user')
+class UserController {
+
+  @RequestMapping(method = GET, value = '/{username}')
+  ResponseEntity findUser(@PathVariable String username) {
+    def user = [
+        'name'    : 'Some name',
+        'lastName': 'Some last name',
+        'address' : 'Some address'
+    ]
+    ResponseEntity.ok(user)
+  }
+
+}
