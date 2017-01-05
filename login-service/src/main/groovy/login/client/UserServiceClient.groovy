8d1e362c30bf8bb40f710aeeeb91ebf377bb0db5
Camilo Rivera <Camilo.AR.Damelines@nab.com.au>
2017-01-05 10:22:36 -0500

    Added configuration for user service endpoint

--- login-service/src/main/groovy/login/client/UserServiceClient.groovy
+++ login-service/src/main/groovy/login/client/UserServiceClient.groovy
@@ -1,12 +1,25 @@
 package login.client
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class UserServiceClient {
+
+  @Value('${services.user.host}')
+  String host
+
+  @Value('${services.user.port}')
+  String port
+
+  @Value('${services.user.endpoints.findUser}')
+  String findUserEndpoint
+
   Map findUser(String username) {
+    def endpoint = findUserEndpoint.replace('{username}', username)
+    def url = "http://$host:$port$endpoint"
     return [
-        message: "User: $username".toString()
+        message: url.toString()
     ]
   }
 }
